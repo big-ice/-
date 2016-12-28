c9ffe24d64767a8bb17ac089a3761d19f8e43e68
big-ice <han@hantekiMacBook-Pro.local>
2016-12-28 16:22:25 +0800

    Initial Commit

---
+++ 计划狗/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  计划狗
+//
+//  Created by 潘若冰 on 2016/12/28.
+//  Copyright © 2016年 潘若冰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
